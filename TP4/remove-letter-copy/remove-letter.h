04217f960d8ddc5bad28415996c46b3f01735944
Felix Lusseau <felix.lusseau@etu.unistra.fr>
2021-10-15 09:32:03 +0200

    ex résolu

---
+++ TP4/remove-letter-copy/remove-letter.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char * removeLetter(char * chaineinit, char carac)
+{
+    char * chaine = malloc(strlen(chaineinit)+1);
+    if (chaine==NULL)
+        return NULL;
+    strcpy(chaine,chaineinit);
+    for (int i=0; chaine[i]!='\0'; i++)
+    {
+        if (chaine[i] == carac)
+        {
+            for (int k=i; chaine[k]!='\0'; k++)
+            chaine[k]=chaine[k+1];
+            i--;
+        } 
+    }
+    return chaine;
+}
