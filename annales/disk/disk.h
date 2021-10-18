01a35f79b3c9bebe3159165f1e1cd3cfff9caf6a
Felix Lusseau <felix.lusseau@etu.unistra.fr>
2021-10-19 00:18:24 +0200

    ex résolu

--- annales/disk/disk.h
+++ annales/disk/disk.h
@@ -21,16 +21,30 @@ char * disk(int d){
 
 char * disk_text(char * texte){
     int d = strlen(texte);
-    d=floor(sqrt(d)+1);
+    d=floor(sqrt(d)+2);
+    char * textec=malloc(1000*sizeof(char));
+    if (textec==NULL)
+        return NULL;
+    char point[1]={"."};
+    strcpy(textec, texte);
+    for (int j=1; j<=d+1; j++){
+        strcat(textec, point);
+    }
     char * chaine = malloc(1000*sizeof(char));
     if (chaine==NULL)
         return NULL;
+    int k=0;
     for (int y=0; y<d; y++){
         for (int x=0; x<d; x++){
-            if (((d/2.0-0.5) - x)*((d/2.0-0.5) - x) + ((d/2.0-0.5) - y)*((d/2.0-0.5) - y) < d*d/4.0 && x+y<(int)strlen(texte))
-                strcat(chaine, &(texte[x+y]));
-            else if (((d/2.0-0.5) - x)*((d/2.0-0.5) - x) + ((d/2.0-0.5) - y)*((d/2.0-0.5) - y) < d*d/4.0 && x+y>(int)strlen(texte))
+            if (((d/2.0-0.5) - x)*((d/2.0-0.5) - x) + ((d/2.0-0.5) - y)*((d/2.0-0.5) - y) < d*d/4.0){
+                char chainetmp[0];
+                chainetmp[0]=textec[k];
+                strcat(chaine, chainetmp);
+                k++;}
+            /*else if (((d/2.0-0.5) - x)*((d/2.0-0.5) - x) + ((d/2.0-0.5) - y)*((d/2.0-0.5) - y) < d*d/4.0 && k>(int)strlen(texte)){
                 strcat(chaine, ".");
+                k++;
+            }*/
             else 
             strcat(chaine, " ");
         }
--- annales/disk/mestests.c
+++ annales/disk/mestests.c
@@ -4,7 +4,7 @@
 int main(void)
 {
   printf("%s", disk(10));
-  //printf("%s", disk_text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."));
-    printf("%s", disk_text("Lorem "));
+  printf("%s", disk_text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."));
+//printf("%s", disk_text("Lorem "));
 
 }
