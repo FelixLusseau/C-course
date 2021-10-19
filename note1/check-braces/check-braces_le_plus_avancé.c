615490dc33f7c91b2147aac85543044018ff0371
Felix Lusseau <felix.lusseau@etu.unistra.fr>
2021-10-19 10:27:35 +0200

    ex résolu

---
+++ note1/check-braces/check-braces_le_plus_avancé.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(){
+    char str[1000];
+    int nbo = 0;
+    int posf = 0;
+    char tmp[1000];
+    while (fgets(tmp, 1000, stdin)){
+        strcat(str, tmp);
+    }
+    printf("%s\n", str);
+    for (int i=0; str[i]!='\0'; i++){
+        if (str[i]=='{')
+            nbo++;
+        else if (str[i]=='}'){
+            if (nbo>0)
+                nbo--;
+            else{
+                posf=i;
+                printf("%s", str);
+                for (int k=0; k<posf; k++)
+                    printf(" ");
+                printf("^ cette accolade fermante est en trop.\n");
+            }
+        }
+    }
+    if (nbo>0)
+        printf("Il manque au moins un accolade ouvrante.\n");
+    return 0;
+}
