2b77f420cbeda1dd88d202bf007e7c2e7f095600
Felix Lusseau <felix.lusseau@etu.unistra.fr>
2021-11-11 00:37:35 +0100

    ex résolu

--- annales/disk/disk.h
+++ annales/disk/disk.h
@@ -4,14 +4,17 @@
 #include <math.h>
 
 char * disk(int d){
-    char * chaine = malloc(1000*sizeof(char));
+    char * chaine = malloc(10000*sizeof(char));
     if (chaine==NULL)
         return NULL;
+    chaine[0] = '\0';
     for (int y=0; y<d; y++){
+        int flag=0;
         for (int x=0; x<d; x++){
-            if (((d/2.0-0.5) - x)*((d/2.0-0.5) - x) + ((d/2.0-0.5) - y)*((d/2.0-0.5) - y) < d*d/4.0)
+            if (((d/2.0-0.5) - x)*((d/2.0-0.5) - x) + ((d/2.0-0.5) - y)*((d/2.0-0.5) - y) < d*d/4.0){
                 strcat(chaine, "#");
-            else 
+                flag=1;}
+            else if (flag!=1)
                 strcat(chaine, " ");
         }
         strcat(chaine, "\n");
@@ -22,30 +25,41 @@ char * disk(int d){
 char * disk_text(char * texte){
     int d = strlen(texte);
     d=floor(sqrt(d)+2);
-    char * textec=malloc(1000*sizeof(char));
+    char point[2]={'.'};
+    //printf("\n%c\n", point[0]);
+    /* char * textec=malloc(10000*sizeof(char));
     if (textec==NULL)
         return NULL;
-    char point[1]={"."};
-    strcpy(textec, texte);
+    textec[0] = '\0';
+    strcpy(textec, texte); */
+    //texte[strlen(texte) - 1] = '\0';
     for (int j=1; j<=d+1; j++){
-        strcat(textec, point);
+        strcat(texte, point);
+    //    printf("%s\n", texte);
     }
     char * chaine = malloc(1000*sizeof(char));
     if (chaine==NULL)
         return NULL;
+    chaine[0] = '\0';
     int k=0;
     for (int y=0; y<d; y++){
+        int flag=0;
         for (int x=0; x<d; x++){
             if (((d/2.0-0.5) - x)*((d/2.0-0.5) - x) + ((d/2.0-0.5) - y)*((d/2.0-0.5) - y) < d*d/4.0){
                 char chainetmp[0];
-                chainetmp[0]=textec[k];
+                printf("textek %c\n", texte[k]);
+                chainetmp[0]=texte[k];
+                printf("chainetmp %s\n", chainetmp);
                 strcat(chaine, chainetmp);
-                k++;}
+                k++;
+                flag=1;
+                printf("chaine %s\n\n", chaine);
+                printf("chainetmp2 %s\n", chainetmp);}
             /*else if (((d/2.0-0.5) - x)*((d/2.0-0.5) - x) + ((d/2.0-0.5) - y)*((d/2.0-0.5) - y) < d*d/4.0 && k>(int)strlen(texte)){
                 strcat(chaine, ".");
                 k++;
             }*/
-            else 
+            else if (flag!=1)
                 strcat(chaine, " ");
         }
         strcat(chaine, "\n");
