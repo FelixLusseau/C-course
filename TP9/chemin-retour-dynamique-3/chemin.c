aec422633924bc63c7d3c1179033f7762dada790
Felix Lusseau <felix.lusseau@etu.unistra.fr>
2021-11-15 23:09:08 +0100

    ex résolu

--- TP9/chemin-retour-dynamique-3/chemin.c
+++ TP9/chemin-retour-dynamique-3/chemin.c
@@ -1,37 +1,24 @@
-# include <stdio.h>
-#include <tps_stack.h>
+#include <stdio.h>
 
-int main(){
-    tps_stack pile = tps_stack_create();
-    int mvt;
-    while (1){
-        scanf("%i", &mvt);
-        tps_stack_push(pile, mvt);
-        if (mvt==0)
-            break;
-    }    
-    int size_pile=tps_stack_size(pile);
-    //printf("%i\n", size_pile);
-    for (int i=0; i<size_pile; i++){
-        switch (tps_stack_pop(pile))
+void action(void)
+{
+    int m;
+    scanf("%i", &m);
+    if (m!=0)
+    {
+        action();
+        switch (m)
         {
-        case 1:
-            printf("2\n");
-            break;
-        case 2:
-            printf("1\n");
-            break;
-        case 3:
-            printf("3\n");
-            break;
-        case 4:
-            printf("5\n");
-            break;
-        case 5:
-            printf("4\n");
-            break;
+        case 4: printf("5\n"); break;
+        case 5: printf("4\n"); break;
+        case 1: printf("2\n"); break;
+        case 2: printf("1\n"); break;
+        case 3: printf("3\n"); break;
         }
     }
-    tps_stack_free(pile);
-    return 0;
+}
+
+int main()
+{
+    action();
 }
