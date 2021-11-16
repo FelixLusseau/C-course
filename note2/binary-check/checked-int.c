4132b63a7920a26d341d33c21f0edf6ed3cadb95
Felix Lusseau <felix.lusseau@etu.unistra.fr>
2021-11-16 17:26:32 +0100

    ex résolu

--- note2/binary-check/checked-int.c
+++ note2/binary-check/checked-int.c
@@ -2,28 +2,11 @@
 #include <stdint.h>
 #include <stdio.h>
 
-void affichebin(uint64_t n)
-{
-	unsigned bit = 0 ;
-	unsigned mask = 1 ;
-	for (int i = 0 ; i < 64 ; ++i)
-	{
-		bit = (n & mask) >> i ;
-		printf("%d", bit) ;
-		mask <<= 1 ;
-	}
-}
-
 uint64_t ci_encode(uint32_t value){
     uint64_t value64 = 0;
     value64=value64 | value;
     value64=value64 | (value64<<32);
-    /* printf("valeur 32 :");
-    affichebin(value);
-    printf("\nvaleur 64 :");
-    affichebin(value64);
-    printf("\n");
-    printf("valeur 32 : %u\n", value);
+    /*printf("valeur 32 : %u\n", value);
     printf("valeur 64 : %lu\n", value64); */
     return value64;
 }
@@ -31,10 +14,13 @@ uint64_t ci_encode(uint32_t value){
 int ci_check(uint64_t value){
     uint32_t value32=(value & 0xFFFF);
     value=value>>32;
-    if (value32==(uint32_t)value){
-        return 0;
+    //printf("ligne 35 : %u %u\n", value32, (uint32_t)value);
+    if (value32&value){
+        printf("Vrai\n");
+        return 1;
     }
-    return 1;
+    printf("faux\n");
+    return 0;
 }
 
 uint32_t ci_decode(uint64_t value, int *ok){
