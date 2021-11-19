354dec4659f5cb15fa350e61743e168ba4e02b9a
Felix Lusseau <felix.lusseau@etu.unistra.fr>
2021-11-19 13:57:53 +0100

    ex résolu

---
+++ TP10/convert/convert.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+# include <math.h>
+
+int main(int argc, char ** argv){
+    if (argc<3){
+        fprintf(stderr, "Il faut entrer ./convert FICHIER-D'ENTREE FICHIER-DE-SORTIE\n");
+        return 1;
+    }
+    FILE * in = fopen(argv[1], "r");
+    FILE * out = fopen(argv[2], "w");
+    float degc;
+    float degf;
+    while (fscanf(in, "%f", &degc)!=EOF){
+        degf=(degc*9/5)+32;
+        fprintf(out, "%.2f\n", degf * 100 / 100);
+    }
+    return 0;
+}
