6ab885b8bc2e51960de08e8b50fce10df95001be
Felix Lusseau <felix.lusseau@etu.unistra.fr>
2021-11-24 10:36:00 +0100

    Résoution...

--- TP11/canary-malloc/ca_malloc.c
+++ TP11/canary-malloc/ca_malloc.c
@@ -2,34 +2,32 @@
 #include <stdio.h>
 #include "ca_malloc.h"
 
-int sizeg=0;
-
 void * ca_malloc(int size){
-    sizeg=size;
-    char * tab = malloc(size+16);
+    char * tab = malloc(size+16+sizeof(unsigned long long int));
     if (tab==NULL)
         exit(42);
-    for (int i=0; i<8; i++){
+    for (unsigned long long int i=0+sizeof(unsigned long long int); i<8+sizeof(unsigned long long int); i++){
         tab[i]=6;
     }
-    for (int j=15+size; j>=8+size; j--){
+    for (unsigned long long int j=15+size+sizeof(unsigned long long int); j>=8+size+sizeof(unsigned long long int); j--){
         tab[j]=6;
     }
-    return (int*)(tab+8);
+    *(unsigned long long int*)tab=size;
+    return (int*)(tab+8+sizeof(unsigned long long int));
 }
 
 void ca_free(int * pointeur){
-    char * pointeurbis = (char*)pointeur-8;
-    /* for (int k=0; k<16+sizeg; k++)
-        printf("%i\n", pointeurbis[k]); */
-    for (int i=0; i<8; i++){
+    char * pointeurbis = (char*)pointeur-8-sizeof(unsigned long long int);
+    for (unsigned long long int k=0; k<16+(*(unsigned long long int*)pointeurbis); k++)
+        printf("%i\n", pointeurbis[k]);
+    for (unsigned long long int i=0+sizeof(unsigned long long int); i<8+sizeof(unsigned long long int); i++){
         //fprintf(stderr, "%i\n", pointeurbis[i]);
         if ((char)pointeurbis[i]!=6){
             fprintf(stderr, "/!\\ un canary a été modifié !!");
             break;
         }
     }
-    for (int j=15+sizeg; j>=8+sizeg; j--){
+    for (unsigned long long int j=15+(*(unsigned long long int*)pointeurbis)+sizeof(unsigned long long int); j>=8+(*(unsigned long long int*)pointeurbis)+sizeof(unsigned long long int); j--){
         //fprintf(stderr, "%i\n", pointeurbis[j]);
         if ((char)pointeurbis[j]!=6){
             fprintf(stderr, "/!\\ un canary a été modifié !!");
